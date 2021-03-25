c3eac75d9b7f292330d108c26c6db3940ddd292d
Hendrick <hendrickwijaya21@gmail.com>
2021-03-25 16:38:30 +0700

    SHowing the modal just once

--- Scenario 1 - Capture emails/js/app.js
+++ Scenario 1 - Capture emails/js/app.js
@@ -1,13 +1,22 @@
 window.onload = function(){
+  let emailState = false
   let emailModal = document.getElementsByClassName('email-modal')[0]
   
   let closeModal = document.getElementsByClassName('email-modal__close-btn')[0]
+
+  let showModal = () => {
+    if(emailState == false){
+      emailModal.classList.add('email-modal--visible')
+      emailState = true;
+    }
+  }
 
   closeModal.addEventListener('click', () => {
     emailModal.classList.remove('email-modal--visible')
   })
 
   document.body.addEventListener('mouseleave', () => {
-    emailModal.classList.add('email-modal--visible')
+    showModal()
   })
+  
 }
